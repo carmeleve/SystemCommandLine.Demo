a6f894edb951b4e52c71d13fc58ffee8804992bc
Carmel Eve <Carmel.Eve@endjin.com>
2020-08-03 10:58:34 +0100

    Added demo tool

---
+++ SystemCommandLine.Demo/GreetCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace SystemCommandLine.Demo
+{
+    public class GreetCommand : Command
+    {
+        private readonly GreetOptions options;
+
+        public GreetCommand(GreetOptions options)
+           : base("greet", "Says a greeting to the specified person.")
+        {
+            var name = new Option<string>("name")
+            {
+                Name = "name",
+                Description = "The name of the person to greet.",
+                IsRequired = true
+            };
+
+            this.AddOption(name);
+
+            this.Handler = CommandHandler.Create(
+                (string name) => this.HandleCommand(name));
+            this.options = options;
+        }
+
+        private int HandleCommand(string name)
+        {
+            try
+            {
+                Console.WriteLine($"{this.options.Greeting} {name}!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
